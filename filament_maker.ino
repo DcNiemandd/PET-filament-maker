c763475dd77825aabae19adee13e6779177fd191
Miloš Musílek <milos.musilek@two.do>
2023-05-29 00:28:52 +0200

    my comments and hopefully helpfull information

--- filament_maker.ino
+++ filament_maker.ino
@@ -1,36 +1,44 @@
 #include <AccelStepper.h>
-#include "HCMotor.h" //Include HCMotor Code Library
+#include "HCMotor.h"
 
-//HCMotor
-#define MOTOR_PIN 6 // Assign to PWM/Digital Pin 6
-#define POT_PIN A1 // Set analog pin at A0 for the potentiometer
-HCMotor HCMotor; // Create an instance of our code library
-
-//AccelStepper
-// Define the stepper motor and the pins that is connected to
-AccelStepper stepper1(1, 4, 2); // (Type of driver: with 2 pins, STEP, DIR)
+//Definice hodnot, které se nahradí při buildu
+#define HOT_END_PIN 6
+#define HOT_POT A1
+#define STEPPER_POT A0
+#define STEPPER_MAX_SPEED 500
+//Vytvoření instance motoru (jedná se o třídu - pokročilé programování, třídy mají vlastní proměnné, které si pamatují, a vlastní funkce/metody, které voláš s tečkou - heating.Init())
+HCMotor heating = HCMotor();
+// ^      ^          ^- jedná se o funkci, která vrátí instanci/proměnnou/objekt, nazývá se constructor
+// |      ------------- název proměnné
+// -------------------- typ proměnné - stejný název jako constructor
+// !!! předtím to byl jiný zápis toho samého, jen jsem to rozepsal, aby bylo vidět, jak se změnilo následující
+// možnosti zápisu volání constructoru:
+// AccelStepper stepper1(1, 4, 2); // == direct call
+// nebo
+AccelStepper stepper1 = AccelStepper(1, 4, 2); // == assignment call
 
 void setup()
 {
-  //HCMotor
-  HCMotor.Init(); //Initialise our library
-  HCMotor.attach(0, DCMOTOR, MOTOR_PIN); // Attach our motor to 0 to digital pin 6
-  HCMotor.DutyCycle(0, 100);
+  // Hot end inicializace
+  heating.Init(); //prvotni nastaveni
+  heating.attach(0, DCMOTOR, HOT_END_PIN); //ukládání hodnot do třídních proměnných viz. výše
+  heating.DutyCycle(0, 100); // nastavení hodnot pro 0 V a max V. Kdyby jsi zde nastavil (0, 1024), tak to v loopu nemusíš mapovat
   
-  //AccelStepper
-  // Set maximum speed value for the stepper
-  stepper1.setMaxSpeed(500);
+  // Navijak
+  stepper1.setMaxSpeed(STEPPER_MAX_SPEED);
 }
 void loop()
 {
-  //HCMotor
-  int Speed;
-  Speed = map(analogRead(POT_PIN), 0, 1024, 0, 100); //Reading the A0 pin to determine the position of the pot.
-  //mapping the motor which could be 0 - 1024 and reduce down to match the cyccle range of 0 to 100
-  HCMotor.OnTime(0, Speed); // Set the duty cycle to match the position
-  
-  //AccelStepper
-  stepper1.setSpeed((analogRead(A0)));
-  // Step the motor with a constant speed previously set by setSpeed();
+  // Hot end
+  int heat_power;
+  heat_power = map(analogRead(POT_PIN), 0, 1024, 0, 100);
+  // zde mohlo být i rovnou:
+  // int Speed = map(...
+  heating.OnTime(0, heat_power);
+
+  // Navijak
+  // stepper1.setSpeed((analogRead(STEPPER_POT))); // namapovat??? takhle využíváš jen polovinu potáku ((500/1024)*100) %
+  int stepper_speed = map(analogRead(STEPPER_POT), 0, 1024, 0, STEPPER_MAX_SPEED); // tresnicka na dortu je přemístění čísla, které se opakuje do definů. tomu se říká magická konstanta, takhle to má název, který to popisuje
+  stepper1.setSpeed(stepper_speed);
   stepper1.runSpeed();
 }
